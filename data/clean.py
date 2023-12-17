c765c738ff0434d4e0306c6cc4c4659b74a2ed0b
Zygmut <deltas.game@gmail.com>
2023-12-17 14:00:17 +0100

    Fix dataset

--- data/clean.py
+++ data/clean.py
@@ -3,8 +3,10 @@ with open("./heart_data.csv", "r") as file:
 
 c_lines = [lines[0][:-1]]
 for line in lines[1:]:
-    cleaned_line = (line[1:-2]).replace('""', '"')
+    cleaned_line = line[1:] if line[0] == '"' else line
+    cleaned_line = cleaned_line[:-2] if line[-2] == '"' else cleaned_line[:-1]
+    cleaned_line = cleaned_line.replace('""', '"')
     c_lines.append(cleaned_line)
 
-with open("./cleaned_data.csv", "w") as file:
+with open("./clean_heart_data.csv", "w") as file:
     file.write("\n".join(c_lines))
